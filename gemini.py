6c36c9ae5fc55400a501aec92cd08f5c96eb4fee
RA4Z <rraz639@gmail.com>
2024-05-24 15:17:13 -0300

    Melhorando output da IA

--- gemini.py
+++ gemini.py
@@ -54,9 +54,9 @@ class GeminiAI():
       Preste atenção às informações no histórico de conversas;
       JAMAIS CITE A EXISTÊNCIA DO HISTÓRICO DE NOSSAS CONVERSAS;
       Busque informações sobre o procedimento do indicador: {message};
-      Quando aparecer escrito ANO_ATUAL, subtitua por {date.today().year}, quando aparecer MES_ATUAL, substitua por {date.today().month:02}, quando aparecer SEMANA_ATUAL, substitua por {date.today().isocalendar().week}, quando aparecer ULTIMO_DIA_UTIL, substituia por {ultimo_dia_util(date.today())};
+      Quando aparecer escrito ANO_ATUAL, subtitua por {date.today().year}, quando aparecer MES_ATUAL, substitua por {date.today().month:02}, quando aparecer SEMANA_ATUAL, substitua por {date.today().isocalendar().week}, quando aparecer ULTIMO_DIA_UTIL, substitua por {ultimo_dia_util(date.today())}, quando aparecer DIA_ATUAL, substitua por {date.today().day:02};
       Separe o passo a passo para atualizar o indicador em vários tópicos, não fazendo uso de subtópicos, deve estar escrito <topico> na frente de cada tópico;
-      Crie um resumo sobre o indicador correspondente ao comando, também informando a última data de atualização do mesmo e se ele está com o status Pendente ou Realizado;
+      Crie um resumo sobre o indicador correspondente ao comando, também informando a última data de atualização do mesmo e se ele está com o status Pendente ou Realizado, além disso mostre o caminho do documento word com o respectivo procedimento;
       
       Siga o modelo abaixo para o output:
       
